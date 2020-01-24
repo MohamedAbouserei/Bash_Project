5d0efe12ed1e13d7ab927d18e5345bdc44716856
samargooda <samarmahmoud368@yahoo.com>
2020-01-24 03:47:33 +0200

    validation of table name

--- s1.sh
+++ s1.sh
@@ -1,4 +1,17 @@
 #!/bin/bash
+echo "plz enter the table name"
+flage=1
+while [ $flage -gt 0 ] 
+do
+read tableName
+if [[ ($tableName == 'number') || ( $tableName == 'string') || ($tableName == 'text') ]]
+then
+echo "plz enter correct name"
+else 
+touch $tableName
+flage=0
+fi
+done
 echo "plz enter the number of columns "
 read num
 echo "plz enter name of columns with datatype sperated with :"
@@ -10,17 +23,13 @@ IFS=':'
 read -ra NAMES <<< "$str"
 echo ${NAMES[1]}
 
-if [[ ("${NAMES[1]}" == 'number') || ( "${NAMES[1]}" == 'string') || ("${NAMES[1]}" == 'text') ]]
+if [[ ("${NAMES[1]}" == 'number') || ( "${NAMES[1]}" == 'string') || ("${NAMES[1]}" == 'tex2t') ]]
 then
 array[i]=$str
+
 else
 echo "plz enter correct data type"
+i=$i-1
 fi
 done
-for (( i=1 ; i <= num ; i++))
-do 
-echo "success"
 echo "${array[@]}" > student
-done
-
-
